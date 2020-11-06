3cd8c3532647e4ae79b1dcdaf6ea5c8b99800f6d
Huzaifa Ahmed <ahmedhuzaifa13@gmail.com>
2020-11-06 18:17:04 +0500

    fixing issues

--- out/extension.js
+++ out/extension.js
@@ -4,8 +4,7 @@ exports.deactivate = exports.activate = void 0;
 const vscode_1 = require("vscode");
 const commands_1 = require("./commands");
 function activate(context) {
-    context.subscriptions.push(vscode_1.commands.registerCommand('react-provider.genFuncComp', (uri) => commands_1.newComponent(uri, 'functional')));
-    context.subscriptions.push(vscode_1.commands.registerCommand('react-provider.genClassComp', (uri) => commands_1.newComponent(uri, 'class')));
+    context.subscriptions.push(vscode_1.commands.registerCommand('react-provider.genFuncComp', (uri) => commands_1.newComponent(uri, 'functional')), vscode_1.commands.registerCommand('react-provider.genClassComp', (uri) => commands_1.newComponent(uri, 'class')));
 }
 exports.activate = activate;
 //huzaifa-ahmed.react-provider
